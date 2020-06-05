a39f81e66588f4f3410a5b48fc8a5763898828e4
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-06-05 14:25:31 +0530

    Updates

---
+++ precache-manifest.628dd1efb31b695fcddfe675e94e0ded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60e5f742279ddecb7d13fde81e39b5a",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "76acf4391623e1ca13ae",
+    "url": "/website/static/css/main.96742132.chunk.css"
+  },
+  {
+    "revision": "2c8cf81d2f57204ff9ba",
+    "url": "/website/static/js/2.7e65fb7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/website/static/js/2.7e65fb7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76acf4391623e1ca13ae",
+    "url": "/website/static/js/main.1093df13.chunk.js"
+  },
+  {
+    "revision": "f5e21e0f0b6dc735d2ce",
+    "url": "/website/static/js/runtime-main.f1541fab.js"
+  }
+]);
